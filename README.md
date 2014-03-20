3135df6c1a15b9878b8b1f6d312ed93ad592eb29
sundar <sundar@sundar-TravelMate-5720.(none)>
2014-03-20 10:32:12 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Yet another FizzBuzz, using Java 8 lambda and streams - but using Nashorn
---
+++ fizzbuzz.js
@@ -0,0 +1,10 @@
+var IntStream = Java.type("java.util.stream.IntStream");
+
+function ifmod(m, r, f) {
+    return function(i) { return i%m == 0? r : f(i) }
+}
+
+IntStream.rangeClosed(1, 100).
+   mapToObj(
+      ifmod(15, "FizzBuzz", ifmod(5, "Buzz", ifmod(3, "Fizz", String))))
+      .forEach(print);
