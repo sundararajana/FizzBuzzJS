dea0c870f69957926178147946b167e730a0198b
sundar <sundar@sundar-TravelMate-5720.(none)>
2014-03-20 10:45:29 +0530

    space around %

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,7 +1,7 @@
 var IntStream = Java.type("java.util.stream.IntStream");
 
 function ifmod(m, r, f) {
-    return function(i) { return i%m == 0? r : f(i) }
+    return function(i) { return i % m == 0? r : f(i) }
 }
 
 IntStream.rangeClosed(1, 100).
